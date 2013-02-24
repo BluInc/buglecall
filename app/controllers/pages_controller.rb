f1516a56e7f781c511e96739214b735541bf3ee7
Joseph Viscomi <jjviscomi@gmail.com>
2013-02-24 02:32:34 -0500

    added some comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user! #Devise forces the user to be authenticated first.
   #load_and_authorize_resource :only => [:index, :show] #cancan support for restful resources
 
   def index
+  	# Example of using cancan
   	if can? :read, PagesController
   	  render action: "index"
   	else
