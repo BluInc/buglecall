5fd6dcb880389670afba7697f470eae051bcd416
Joseph Viscomi <jjviscomi@gmail.com>
2013-02-24 01:51:25 -0500

    Fixed cancan error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
+  #load_and_authorize_resource #cancan support for restful resources
 
   def index
   	render action: "index"
