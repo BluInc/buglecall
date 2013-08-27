5a098b4490c286af9d50acdc17a9e862aa5f590f
BluInc <bowen7111@gmail.com>
2013-08-27 12:34:22 -0400

    adding user.name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
   #load_and_authorize_resource :only => [:index, :show] #cancan support for restful resources
 
   def index
+  	if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   	render action: "index"
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,12 @@ class User < ActiveRecord::Base
     relationships.find_by(followed_id: other_user.id).destroy!
   end
 
+  # Micropost status feed
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   has_attached_file :avatar, styles: {
     small:  '25x25>',
     thumb:  '100x100>',
