4c68f9b1a2b96a93e0514477452a3b0220947509
BluInc <bowen7111@gmail.com>
2013-08-28 11:56:07 -0400

    microdel

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   #before_action :signed_in_user, only: [:create, :destroy]
   #include ActiveModel::ForbiddenAttributesProtection
+  before_filter :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,11 +14,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
